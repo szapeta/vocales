caab361aa18cf3d9bdbcf8cb72f5c50c2128d4c6
szapeta <2375894860101@ingenieria.usac.edu.gt>
2021-05-30 17:12:47 -0600

    version con estadisticas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 let bot1 = document.getElementById("bot1");
 let bot2 = document.getElementById("bot2");
 let bot3 = document.getElementById("bot3");
+let labC = document.getElementById("labcorrectas");
+let labI = document.getElementById("labincorrectas");
+
 let botMaster = document.getElementById("botMaster");
 
 let letras = ["m", "p", "l"];
@@ -8,25 +11,48 @@ let vocales = ["a", "e", "i", "o", "u"];
 let botones = [bot1, bot2, bot3];
 let audio;
 
+let numCorrectas = 0;
+let numIncorrectas = 0;
+let contador = 0;
+let silabaMaster = "";
+
 const funcambiar = () =>{
 
     let i = 0;
     let max = botones.length;
     let silaba;
 
+
+    if(contador > 0){
+
+        let idSelected = event.srcElement.id;
+        silabaSelected = document.getElementById(idSelected).firstChild.data;
+        
+        if(silabaSelected == silabaMaster){
+            numCorrectas = numCorrectas +1;
+        }else{
+            numIncorrectas = numIncorrectas +1;
+        }
+
+        labC.firstChild.data = numCorrectas;
+        labI.firstChild.data = numIncorrectas;
+        
+    }
+
+    contador = contador +1;
+
     while(i<max){
 
-        let indice = Math.floor(Math.random()*letras.length);
+        let indiceConsonante = Math.floor(Math.random() * letras.length);
         let indiceVocal = Math.floor(Math.random() * vocales.length);
-
-        silaba = letras[indice] + vocales[indiceVocal];
+        silaba = letras[indiceConsonante] + vocales[indiceVocal];
+        
         botones[i].firstChild.data = silaba;
         i = i+1;
     }
 
-    let indiceMaster = Math.floor(Math.random()*botones.length);
-    let silabaMaster = botones[indiceMaster].firstChild.data;
-    console.log(silabaMaster);
+    let indiceMaster = Math.floor(Math.random() * max);
+    silabaMaster = botones[indiceMaster].firstChild.data;
     buscarSilaba(silabaMaster)
 }
 
